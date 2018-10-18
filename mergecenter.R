1f1f143af21a73f0a0497b1268c0b1594a9c8b68
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 13:28:56 -0400

    Saving mergecenter.R

--- mergecenter.R
+++ mergecenter.R
@@ -1,1 +1,11 @@
 
+stateName <- state.name
+area <- state.area
+center <- state.center
+df <- data.frame(stateName,area,center) # creation of new data frame
+
+mergedf <- merge(mergedData,df, by.x = "StateName", by.y = "stateName") # merging it with final data frame 
+View(mergedf)
+
+mergedf$StateName <- tolower(mergedf$StateName) #because ggplot,ggmap supports only lower case
+mergedf
